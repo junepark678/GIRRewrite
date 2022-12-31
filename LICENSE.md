e07579c07b423a33922e1509a9b24d1ed9acc3d2
June P <40409848+junepark678@users.noreply.github.com>
2022-12-31 23:03:49 +0900

    steal fixsocials from botsu idk why

--- LICENSE.md
+++ LICENSE.md
@@ -1,22 +1,11 @@
+--2022-12-31 23:03:09--  https://raw.githubusercontent.com/Pearl-Codes/Botsu/main/LICENSE
+Resolving raw.githubusercontent.com (raw.githubusercontent.com)... 185.199.111.133, 185.199.108.133, 185.199.110.133, ...
+Connecting to raw.githubusercontent.com (raw.githubusercontent.com)|185.199.111.133|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 35149 (34K) [text/plain]
+Saving to: ‘LICENSE’
 
-The MIT License (MIT)
+     0K .......... .......... .......... ....                 100% 26.0M=0.001s
 
-Copyright (c) 2022 Aamir Farooq
+2022-12-31 23:03:09 (26.0 MB/s) - ‘LICENSE’ saved [35149/35149]
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
--- cogs/monitors/misc/fixsocials.py
+++ cogs/monitors/misc/fixsocials.py
@@ -18,27 +18,36 @@ class FixSocials(commands.Cog):
         # regex for instagram urls
         self.instagram_pattern = re.compile(r"(https:\/\/(www.)?instagram\.com\/(?:p|reel)\/([^/?#&]+))\/")
 
+        # twitter regex
+        self.twitter_pattern = re.compile(r"https:\/\/(www.)?twitter\.com\/[A-Za-z0-9]+\/status\/[0-9]+", re.IGNORECASE)
+
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        if cfg.aaron_id is None or cfg.aaron_role is None:
-            return
         if not message.guild:
             return
-        if message.guild.id != cfg.guild_id:
-            return
         if message.author.bot:
-            return
-        if message.channel.id != guild_service.get_guild().channel_general:
             return
 
         message_content = message.content.strip("<>")
         if tiktok_match := self.tiktok_pattern.search(message_content):
             link = tiktok_match.group(0)
+            print(f"link found:")
+            print(f"type: TikTok")
+            print(f"link: {link}")
             await self.fix_tiktok(message, link) 
-        elif instagram_match := self.instagram_pattern.search(message_content):
+        if instagram_match := self.instagram_pattern.search(message_content):
             link = instagram_match.group(0)
+            print(f"link found:")
+            print(f"type: Instagram")
+            print(f"link: {link}")
             await self.fix_instagram(message, link)
+        if twitter_match := self.twitter_pattern.search(message_content):
+            link = twitter_match.group(0)
+            print(f"link found:")
+            print(f"type: Twitter")
+            print(f"link: {link}")
+            await self.fix_twitter(message, link)
 
     @cached(ttl=3600)
     async def get_tiktok_redirect(self, link: str):
@@ -61,7 +70,7 @@ class FixSocials(commands.Cog):
         if redirected_url is None:
             return
 
-        await message.reply(f"I hate tiktok but here you go {redirected_url}")
+        await message.reply(f"based tiktok user, seems you forgot to vx that shit. Here you go! {redirected_url}")
         await asyncio.sleep(0.5)
         await message.edit(suppress=True)
 
@@ -70,7 +79,16 @@ class FixSocials(commands.Cog):
         link = link.replace("instagram.com", "ddinstagram.com")
 
         # get video id from link
-        await message.reply(f"I hate instagram but here you go {link}")
+        await message.reply(f"based instagram user, seems you forgot to dd that shit. Here you go! {link}")
+        await asyncio.sleep(0.5)
+        await message.edit(suppress=True)
+
+    async def fix_twitter(self, message: discord.Message, link: str):
+        link = link.replace("www.", "")
+        link = link.replace("twitter.com", "fxtwitter.com")
+
+        # get video id from link
+        await message.reply(f"based twitter user, seems you forgot to fx that shit. Here you go! {link}")
         await asyncio.sleep(0.5)
         await message.edit(suppress=True)
 
